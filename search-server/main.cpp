ab1f3b8f2fe28fe6b20aeb9907d7cc16f0a9a633
martnick <martnick@yandex.ru>
2023-04-16 12:24:17 +0300

    First sprint. Correction

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,30 +1,3 @@
-/* Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-bool IsThree (int number) // Проверяет есть ли в числе цифра 3
-{
-    string numstring = to_string(number);
-    for (char c : numstring)
-        if (c == '3')
-            return true;
-    return false;
-}
-
-int main() {
-    int count = 0; //счётчик чисел
-    for (int i = 1; i<=1000; ++i)
-        if (IsThree(i))
-            ++count; // Считаем числа с цифрой 3
-    cout << count << endl;
-}
-// Насчитал 271 число
-    
-// Закомитьте изменения и отправьте их в свой репозиторий.*/
-
 #include <algorithm>
 #include <iostream>
 #include <set>
@@ -86,10 +59,16 @@ public:
 
     void AddDocument(int document_id, const string& document) {
         const vector<string> words = SplitIntoWordsNoStop(document);
-        int w_size = words.size();
+// ЗАМЕЧАНИЕ: count также как и цикл проходит по всем словам, что не эффективно, к тому же при повторных словах вы будете заново все пересчитывать. проще вычислить до цикла значение веса одного слова и затем прибавлять к ячейке
+   /*   for (const string& word : words) 
+        { 
+            documents_[word][document_id] = (static_cast<double>(count (words.begin(), words.end(), word))/static_cast<double>(w_size));
+        }*/
+// Согласен, поторопился и не проанализировал структуру вычислений, сделал "в лоб", вынес только вычесление w_size за цикл. Исправил по замечаниям:
+        double one_TF = 1.0 / words.size();        
         for (const string& word : words)
         {
-            documents_[word][document_id] = (static_cast<double>(count (words.begin(), words.end(), word))/static_cast<double>(w_size));           
+            documents_[word][document_id] += one_TF;           
         }
         ++document_count_;
     }
@@ -133,7 +112,9 @@ private:
         }
         return words;
     }
-
+// КОМЕНТАРИЙ: можно улучшить и выделить метод обработки слов запроса, который в структурированном виде вернул бы информацию - само слова, признак минуса, признак стопа
+/* Пока не стал исправлять, так как обработка выполняется только в одном месте, не вижу большого смысла в создании структуры, подозреваю правда, что в будущем структура пригодится, придётся переделывать. Изначально сделал отдельную функцию для выделения минус-слов, но потом запихнул всё сюда, видимо нужно мозги перестраивать, чтобы делать более читаемый код. 
+Сделал проверку if (word.size() > 2 && word[0] == '-') (в авторском решении if (text[0] == '-')) для общего случая, так как в строке помимо минус-слов может быть отдельное слово "-", понимаю, что в нашем случае оно не несёт информации и должно попасть в стоп-слова, но тогда оно отсеется позже в else if (!IsStopWord(word)) */
     Query ParseQuery(const string& text) const {
         Query query;
         for (const string& word : SplitIntoWordsNoStop(text)) {
@@ -146,29 +127,37 @@ private:
         }
         return query;
     }
+    //ЗАМЕЧАНИЕ: идф не зависит от слова и от идентификатора, нет смысла на каждом витке его вычислять, это можно сделать до цикла. а еще лучше, если организуете отдельную функцию для вычисления ИДФ
+    // Выделил вычесление IDF в отдельную функцию. Изначально плохо вник в теорию и рассматривал IDF как атрибут документа, поэтому вычислял в цикле, хотя математически, действительно, параметр можно вычмслить сразу.
+    double IDFCalc (const string& word) const { 
+        const int num_found_docs = documents_.at(word).size(); // Количество документов, содержащтих слово word
+        return log ( static_cast<double>(document_count_) / num_found_docs);
+    }
 
     vector<Document> FindAllDocuments(const Query& query) const {
         vector<Document> matched_documents;
         map<int, double> docs_relevance; // <id документа, релевантность документа (TF*IDF)>
         for (const string& word : query.plus_words) {
-            if (documents_.count(word)>0) {
-                double count_docs = static_cast<double>(documents_.at(word).size());
-                for (const pair<int, double>& doc : documents_.at(word))
-                    docs_relevance[doc.first] += doc.second * log(static_cast<double> (document_count_)/count_docs);
+            if (documents_.count(word) > 0) {
+                double word_IDF = IDFCalc (word);
+//ЗАМЕЧАНИЕ: стоит декомпозировать переменную через [переменная1, переменная2]. работать с именованными переменными лучше, чем с first / second
+// Переделал в: const auto& [doc_id, word_TF]. Согласен с замечанием, но сделал изначально const pair<int, double>& doc, потому что так понятна структура данных (мне это больше нравиться при небольшом объёме кода с данной переменной). На "идеальный" вариант const pair<int, double>& [doc_id, word_TF] компилятор выдаёт ошибку.
+                for (const auto& [doc_id, word_TF] : documents_.at(word)) 
+                    docs_relevance[doc_id] += word_TF * word_IDF;
                 }       
             }
         for (const string& word : query.minus_words) {
             if (documents_.count(word)>0)
-                for (const pair<int, double>& doc : documents_.at(word))
-                    if (docs_relevance.count(doc.first))
-                        docs_relevance.erase(doc.first);
+                for (const auto& [doc_id, word_TF] : documents_.at(word))
+                    if (docs_relevance.count(doc_id))
+                        docs_relevance.erase(doc_id);
             }
         for (const pair<int, double>& doc : docs_relevance)
             matched_documents.push_back({doc.first, doc.second});
         return matched_documents;
     }   
 };
-
+// Спасибо за конструктивные замечания, текст программы получается более лаконичным и понятным.
 SearchServer CreateSearchServer() {
     SearchServer search_server;
     search_server.SetStopWords(ReadLine());
